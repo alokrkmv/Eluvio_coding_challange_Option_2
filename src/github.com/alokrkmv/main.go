2d92f7562b33cbe83543ef7e814b10521ae5d723
Alok <alokrkmv12@gmail.com>
2022-01-29 17:03:13 -0500

    Added the logic to write the final output to a file

--- src/github.com/alokrkmv/main.go
+++ src/github.com/alokrkmv/main.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/alokrkmv/fetch_data"
 	"github.com/alokrkmv/read_from_file"
+	"github.com/alokrkmv/writer"
 )
 
 func main() {
 	ids := read_from_file.ReadFromTextFile()
-	// final_res,failed_ids:=fetch_data.GetData(ids)
 	final_res, failed_ids := fetch_data.GetConcurrentData(ids)
-	fmt.Println(final_res)
+	err := writer.WriteOutput(final_res)
+	if err != nil {
+		fmt.Println("Something went wrong in writing response to the output file")
+	}
+	// fmt.Println(final_res)
 	fmt.Println(failed_ids)
 }
