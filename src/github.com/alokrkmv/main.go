842d430582e70aec5da21e4311d08ee49d7a4936
Alok <alokrkmv12@gmail.com>
2022-01-29 18:52:18 -0500

    Fixed some typo in comment

--- src/github.com/alokrkmv/main.go
+++ src/github.com/alokrkmv/main.go
@@ -16,11 +16,11 @@ func main() {
 	end_time := time.Now()
 	total_response_time := end_time.Sub(start_time)
 	meta_map["total_response_time"] = total_response_time
-	go writer.MetaDataWriter(meta_map)
-	err := writer.WriteOutput(final_res)
 
 	// Writing meta data is not a primary task so we will spawn a go routine to
 	// write meta data hence it won't hamper the performance of the actual program
+	go writer.MetaDataWriter(meta_map)
+	err := writer.WriteOutput(final_res)
 
 	if err != nil {
 		fmt.Println("Something went wrong in writing response to the output file")
