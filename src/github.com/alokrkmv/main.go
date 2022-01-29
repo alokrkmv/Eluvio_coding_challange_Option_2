e7b59d0040040d3a9b57646df8990916671c9828
Alok <alokrkmv12@gmail.com>
2022-01-28 22:29:52 -0500

    Minor fix

--- src/github.com/alokrkmv/main.go
+++ src/github.com/alokrkmv/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("Hello World")
+import (
+	"fmt"
+
+	"github.com/alokrkmv/fetch_data"
+	"github.com/alokrkmv/read_from_file"
+)
+
+func main() {
+	ids := read_from_file.ReadFromTextFile()
+	final_res,failed_ids:=fetch_data.GetData(ids)
+	fmt.Println(final_res)
+	fmt.Println(failed_ids)
 }
