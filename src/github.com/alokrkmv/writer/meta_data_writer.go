192ccee1fa9ddac3631fc31f9669285d4ca41d79
Alok <alokrkmv12@gmail.com>
2022-01-30 01:44:12 -0500

    Removed some commented code

--- src/github.com/alokrkmv/writer/meta_data_writer.go
+++ src/github.com/alokrkmv/writer/meta_data_writer.go
@@ -17,17 +17,6 @@ func MetaDataWriter(meta_data map[string]interface{},wg *sync.WaitGroup) {
 	mydir, _ := os.Getwd()
 	// Generate the complete file path to write the data
 	filePath := fmt.Sprintf("%s/%s", mydir, "writer/meta_data.json")
-	// final_res := make(map[string]interface{})
-	// var res_array []interface{}
-	// for key, value := range res {
-	// 	temp_res := map[string]string{
-	// 		"Id":       key,
-	// 		"Response": value,
-	// 	}
-	// 	res_array = append(res_array, temp_res)
-	// }
-	// final_res["result"] = res_array
-	// // Marshaling to json
 	file, _ := json.MarshalIndent(meta_data, "", " ")
 	err := ioutil.WriteFile(filePath, file, 0644)
 	if err != nil {
