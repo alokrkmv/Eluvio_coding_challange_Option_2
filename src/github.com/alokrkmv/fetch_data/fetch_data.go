29a49858929ced48dac032311215ee3e7ac1655e
Alok <alokrkmv12@gmail.com>
2022-01-31 23:11:44 -0500

    Added the logic to generate Auth headers using base64 encoders

--- src/github.com/alokrkmv/fetch_data/fetch_data.go
+++ src/github.com/alokrkmv/fetch_data/fetch_data.go
@@ -23,18 +23,18 @@ var cache = redis.NewClient(&redis.Options{
 	DB:       0,
 })
 
-const BASE_URL = "https://challenges.qluv.io/items/"
+const BASE_id = "https://challenges.qluv.io/items/"
 
 // Intializing the mutex variable to prevent race condition
 var mutex = &sync.RWMutex{}
 
 // A basic get request handler
-func get_request_handler(url string) (res interface{}, is_429 bool, err error) {
+func get_request_handler(id string) (res interface{}, is_429 bool, err error) {
 	client := &http.Client{}
-	final_url := BASE_URL + url
-	req, _ := http.NewRequest("GET", final_url, nil)
-	req.Header.Set("Authorization", "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI")
-
+	final_id := BASE_id + id
+	req, _ := http.NewRequest("GET", final_id, nil)
+	// req.Header.Set("Authorization", "Y1JGMmR2RFpRc211MzdXR2dLNk1UY0w3WGpI")
+	req.Header.Set("Authorization", helpers.AuthHeaderGenerator(id))
 	resp, err := client.Do(req)
 
 	// Log and return the error in case the request fails
@@ -67,8 +67,8 @@ func get_request_handler(url string) (res interface{}, is_429 bool, err error) {
 }
 
 // Function to fetch data from the API concurrently
-func GetConcurrentData(urls []string) (final_res map[string]string, meta_map map[string]interface{}) {
-	// This map conatins the final result
+func GetConcurrentData(ids []string) (final_res map[string]string, meta_map map[string]interface{}) {
+	// This map contains the final result
 	final_res = make(map[string]string)
 	// This map contains various meta data
 	var failed_ids []string
@@ -78,36 +78,37 @@ func GetConcurrentData(urls []string) (final_res map[string]string, meta_map map
 	var count_429 int
 	wg := sync.WaitGroup{}
 	i := 0
-	for i < len(urls) {
-		var spliced_url []string
+	for i < len(ids) {
+		var spliced_id []string
 		// Splicing the array of ids into a set of 5 as the API can't handle more than
 		// five concurrent requests at a time.
 		// This will prevent API from throwing 429 error and also ensure to get
 		// maximum throughput from the API
-		if i+5 < len(urls) {
-			spliced_url = urls[i : i+5]
+		if i+5 < len(ids) {
+			spliced_id = ids[i : i+5]
 		} else {
-			spliced_url = urls[i:]
+			spliced_id = ids[i:]
 		}
 
 		// Filtering any duplicate id to avoid unwanted goroutine spwans
-		spliced_url, duplicate_count, duplicate_ids := helpers.RemoveDuplicateValues(spliced_url)
+		spliced_id, duplicate_count, duplicate_ids := helpers.RemoveDuplicateValues(spliced_id)
 		duplicate_ids_count = duplicate_ids_count + duplicate_count
 		duplicate_id = append(duplicate_id, duplicate_ids...)
-		for _, url := range spliced_url {
+		for _, id := range spliced_id {
 			// If the result is already present in redis cache then serve from cache
 			// This will prevent API calls for duplicate ids
-			val, err := cache.Get(context.TODO(), url).Result()
+			val, err := cache.Get(context.TODO(), id).Result()
 			if err == nil {
-				final_res[url] = val
+				final_res[id] = val
 				duplicate_ids_count++
+				duplicate_id = append(duplicate_id, id)
 				continue
 			}
 			wg.Add(1)
 			// Initialize go routines for concurrent calls.
-			go func(url string) {
+			go func(id string) {
 
-				res, is_429, err := get_request_handler(url)
+				res, is_429, err := get_request_handler(id)
 				if is_429 {
 					count_429++
 				}
@@ -115,12 +116,12 @@ func GetConcurrentData(urls []string) (final_res map[string]string, meta_map map
 					log.Fatal(err)
 					// Adding mutex lock to prevent race condition
 					mutex.Lock()
-					failed_ids = append(failed_ids, url)
+					failed_ids = append(failed_ids, id)
 					mutex.Unlock()
 				}
 				// Adding mutex lock to prevent race condition while writing result to map
 				mutex.Lock()
-				final_res[url] = string(res.([]uint8))
+				final_res[id] = string(res.([]uint8))
 				mutex.Unlock()
 
 				// Once the data is fetched caching it for one hour in redis cache so that
@@ -132,15 +133,15 @@ func GetConcurrentData(urls []string) (final_res map[string]string, meta_map map
 				// We don't need to add any mutex lock while writing to redis cache
 				// as redis is capable of handling concurrent read and writes
 
-				err = cache.Set(context.TODO(), url, string(res.([]uint8)), 1440*time.Second).Err()
+				err = cache.Set(context.TODO(), id, string(res.([]uint8)), 1440*time.Second).Err()
 				mutex.Lock()
-				successful_ids = append(successful_ids, url)
+				successful_ids = append(successful_ids, id)
 				mutex.Unlock()
 				if err != nil {
 					log.Fatal(err)
 				}
 				wg.Done()
-			}(url)
+			}(id)
 
 		}
 		wg.Wait()
@@ -148,17 +149,17 @@ func GetConcurrentData(urls []string) (final_res map[string]string, meta_map map
 	}
 	// Storing unique duplicate ids
 	duplicate_id, _, _ = helpers.RemoveDuplicateValues(duplicate_id)
-
 	meta_map = map[string]interface{}{
-		"total_ids":            urls,
-		"total_ids_count":      len(urls),
-		"failed_ids":           failed_ids,
-		"successful_ids":       successful_ids,
-		"duplicate_ids_count":  duplicate_ids_count,
-		"unique_duplicate_ids": duplicate_id,
-		"count_429":            count_429,
-		"number_of_failed_ids": len(failed_ids),
-		"number_of_api_calls":  len(successful_ids),
+		"total_ids":                            ids,
+		"total_ids_count":                      len(ids),
+		"failed_ids":                           failed_ids,
+		"successful_ids":                       successful_ids,
+		"duplicate_ids_count":                  duplicate_ids_count,
+		"unique_duplicate_ids":                 duplicate_id,
+		"count_429":                            count_429,
+		"number_of_failed_ids":                 len(failed_ids),
+		"number_of_api_calls":                  len(successful_ids),
+		"number_of_repeated_api_calls_avoided": len(ids) - len(successful_ids),
 	}
 	return final_res, meta_map
 }
